749968858861e729977e25afd9d5478d2db2f126
cmhrabi <chrabi@uwaterloo.ca>
2022-08-24 11:59:59 -0400

    Updates

---
+++ precache-manifest.0434a86027f0401775074d39899f17ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9c2c6996072ce660d5427306851e4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f831399ad31c5cf9cb73",
+    "url": "/static/css/main.b8d21e90.chunk.css"
+  },
+  {
+    "revision": "adeb6f77a0a2e2933d5d",
+    "url": "/static/js/2.2aad15ad.chunk.js"
+  },
+  {
+    "revision": "bdaaa5b47ab645eaf8a72fa818aab38a",
+    "url": "/static/js/2.2aad15ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f831399ad31c5cf9cb73",
+    "url": "/static/js/main.87e9f57e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.87e9f57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2401c040861091065be",
+    "url": "/static/js/runtime-main.66f6ec9d.js"
+  }
+]);
